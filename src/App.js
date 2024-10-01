0f8914f30a104700ad16e11674609ec1befd7a43
K Kgaba <222053277@tut4life.ac.za>
2024-10-01 16:01:46 +0200

    added admin header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './screens/Login';
-
+import AdminHeader from './components/AdminHeader';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
       <Routes>
-      <Route exact path='/' element={<Login />} />
+      <Route exact path='/' element={<AdminHeader />} />
       </Routes>
     </Router>
   );
